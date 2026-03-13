90160f5e2c39b8b15588521ebbb99e8d2cf6cdc3
kirill <kirya.abbasov.02@mail.ru>
2026-03-13 13:34:24 +0300

    Добавил модули микросервисов, для начала перенес уже существующий gradle файл в микросервисы

---
+++ btf-service/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ru.tbank"
+version = "0.0.1-SNAPSHOT"
+description = "Project development workshop. Tbank, winter 2026. BTF microservice"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-web-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
--- btf-service/src/main/java/ru/tbank/pp/BackToFrontApplication.java
+++ btf-service/src/main/java/ru/tbank/pp/BackToFrontApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PpApplication {
-
+public class BackToFrontApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PpApplication.class, args);
+		SpringApplication.run(BackToFrontApplication.class, args);
 	}
-
 }
