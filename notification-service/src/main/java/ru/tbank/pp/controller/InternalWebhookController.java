90e6f420f98c31f0567b45635f21f02cd84412db
kirill <kirya.abbasov.02@mail.ru>
2026-03-27 21:32:25 +0300

    Разделение контроллера

--- notification-service/src/main/java/ru/tbank/pp/controller/InternalWebhookController.java
+++ notification-service/src/main/java/ru/tbank/pp/controller/InternalWebhookController.java
@@ -4,24 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.tbank.pp.dto.RequestDto;
 import ru.tbank.pp.service.TgBotService;
 
 @RestController
 @RequiredArgsConstructor
-public class WebhookController {
+public class InternalWebhookController {
     private final TgBotService botService;
-
-    /**
-     * Этот контроллер использует сам телеграм, если отправлять запросы извне работать не будет
-     */
-    @PostMapping("/callback")
-    public BotApiMethod<?> handleUpdate(@RequestBody Update update) {
-        return botService.onWebhookUpdateReceived(update);
-    }
 
     /**
      * Контроллер для наших вебхуков, должен вызываться при уменьшении цены до порога, вызывается из основного сервиса
