89e9ce40a0557327c195a059fe12fe6b5c949c5f
kirill <kirya.abbasov.02@mail.ru>
2026-04-11 20:46:43 +0300

    Исправление клиентов

--- btf-service/src/main/java/ru/tbank/pp/client/NotificationClient.java
+++ btf-service/src/main/java/ru/tbank/pp/client/NotificationClient.java
@@ -2,16 +2,17 @@ package ru.tbank.pp.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 import ru.tbank.dto.NotificationRequestDto;
 import ru.tbank.pp.config.RestClientConfig;
 
 @Component
 @RequiredArgsConstructor
 public class NotificationClient {
-    private final RestClientConfig restClientConfig;
+    private final RestClient restClient;
 
     public void sendNotification(NotificationRequestDto notificationRequestDto) {
-        restClientConfig.restClient()
+        restClient
                 .post()
                 .uri("/webhook/send")
                 .body(notificationRequestDto)
--- notification-service/src/main/java/ru/tbank/pp/client/BackendClient.java
+++ notification-service/src/main/java/ru/tbank/pp/client/BackendClient.java
@@ -2,6 +2,7 @@ package ru.tbank.pp.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 import ru.tbank.pp.config.RestClientConfig;
 import ru.tbank.pp.model.ServiceConnectionConnectRequest;
 import ru.tbank.pp.model.ServiceConnectionStatusCheckRequest;
@@ -9,10 +10,10 @@ import ru.tbank.pp.model.ServiceConnectionStatusCheckRequest;
 @Component
 @RequiredArgsConstructor
 public class BackendClient {
-    private final RestClientConfig restClientConfig;
+    private final RestClient restClient;
 
     public boolean checkIfUserExists(ServiceConnectionStatusCheckRequest connectionStatusCheckRequest) {
-        var result =  restClientConfig.restClient()
+        var result =  restClient
                 .post()
                 .uri("/service/check-status")
                 .body(connectionStatusCheckRequest)
@@ -22,7 +23,7 @@ public class BackendClient {
     }
 
     public void connectUserService(ServiceConnectionConnectRequest serviceConnectionConnectRequest) {
-        restClientConfig.restClient()
+        restClient
                 .post()
                 .uri("/service/connect")
                 .body(serviceConnectionConnectRequest)
