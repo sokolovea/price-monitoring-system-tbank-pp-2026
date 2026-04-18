eba29cb35942e073f587c5a43fe779875f800650
kirill <kirya.abbasov.02@mail.ru>
2026-04-18 23:18:18 +0300

    Исправление main

--- integration-service/src/main/java/ru/tbank/pp/integration/IntegrationApplication.java
+++ integration-service/src/main/java/ru/tbank/pp/integration/IntegrationApplication.java
@@ -12,7 +12,7 @@ import ru.tbank.config.KafkaConfig;
 @Import(KafkaConfig.class)
 @ConfigurationPropertiesScan
 public class IntegrationApplication {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(IntegrationApplication.class, args);
     }
 }
